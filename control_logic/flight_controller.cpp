d7adebc0c928ca6c6ea854b3329c4da1f680b253
Alex Rodrigues <aa@aa.com>
2025-07-27 15:49:17 -0500

    one day

--- control_logic/flight_controller.cpp
+++
@@ -1,9 +0,0 @@
-#include "flight_controller.h"
-
-FlightController::FlightController(IHAL* hal) : hal(hal) {}
-
-MotorCommands FlightController::calculate_motor_commands(UserInput input) {
-    SensorData sensor_data = hal->read_sensors();
-    // TODO: Implement PID controller logic here
-    return MotorCommands();
-}
