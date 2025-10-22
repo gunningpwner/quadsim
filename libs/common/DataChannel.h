63aca0b2c862b3eac2654f79d5201189ad21a7ba
A <A@a>
2025-10-22 13:49:48 -0500

    Time to start another refactor of the data manager woohoo. (BREAKS THINGS)

--- libs/common/DataChannel.h
+++ libs/common/DataChannel.h
@@ -1,20 +1,13 @@
 #pragma once
 
-#include <vector>
 #include <algorithm> // For std::min
 #include <type_traits> // For SFINAE/if constexpr
 #include <stdexcept>   // For std::invalid_argument
 #include <string>      // For std::string
 #include <typeinfo>    // For typeid
 
-// Use a more robust check for an embedded ARM GCC environment.
-#ifdef FIRMWARE_BUILD
 #include <array>
 #include <atomic>
-#else
-#include <deque>
-#include <mutex>
-#endif
 
 namespace detail {
     // Type trait to check if a type T has a member function `bool containsNaN() const`.
@@ -30,17 +23,28 @@ namespace detail {
     inline constexpr bool is_nan_checkable_v = is_nan_checkable<T>::value;
 }
 
+/**
+ * @brief A non-templated base interface for DataChannel.
+ * This allows Consumers to hold a reference to a DataChannel without needing to
+ * know its specific buffer size, enabling decoupling.
+ */
 template <typename T>
-class DataChannel {
+class IDataChannel {
 public:
-#ifdef FIRMWARE_BUILD
+    virtual ~IDataChannel() = default;
+    virtual size_t consume(T* sample_buffer, size_t max_samples, unsigned int& last_seen_count, size_t& last_read_index) = 0;
+};
+
+template <typename T, size_t BufferSize>
+class DataChannel : public IDataChannel<T> {
+public:
     // --- Embedded-Safe, Lock-Free Ring Buffer Implementation ---
-    // The buffer_size argument is ignored here, as the size is fixed at compile time for SPMC.
-    explicit DataChannel(size_t /*buffer_size*/) : m_head(0), m_update_count(0) {}
+    // The buffer_size is now a template argument, so no runtime argument is needed.
+    explicit DataChannel() : m_head(0), m_update_count(0) {}
 
     // Throws std::invalid_argument if data contains NaN and T is NaN-checkable.
     void post(const T& data) {
-        // On embedded, we cannot throw exceptions from an ISR.
+        // In a unified build, we decide how to handle NaNs.
         // The check is performed, but we simply don't post the data if it's NaN.
         if constexpr (detail::is_nan_checkable_v<T>) {
             if (data.containsNaN()) {
@@ -49,7 +53,7 @@ public:
         }
 
         size_t current_head = m_head.load(std::memory_order_relaxed);
-        size_t next_head = (current_head + 1) % COMPILE_TIME_BUFFER_SIZE;
+        size_t next_head = (current_head + 1) % BufferSize;
 
         // In this SPSC queue, if the head catches the tail, we overwrite old data.
         // This is a common strategy for sensor data where the latest is most important.
@@ -59,7 +63,7 @@ public:
     }
 
     // New consume method for SPMC. Returns number of samples read.
-    size_t consume(T* sample_buffer, size_t max_samples, unsigned int& last_seen_count, size_t& last_read_index) {
+    size_t consume(T* sample_buffer, size_t max_samples, unsigned int& last_seen_count, size_t& last_read_index) override {
         unsigned int current_update_count = m_update_count.load(std::memory_order_acquire);
         if (last_seen_count == current_update_count) {
             return 0; // No new data
@@ -68,18 +72,18 @@ public:
         // Calculate how many samples have been written since we last checked.
         // This is the key to detecting a buffer overrun.
         unsigned int samples_missed = current_update_count - last_seen_count;
-        if (samples_missed > COMPILE_TIME_BUFFER_SIZE) {
+        if (samples_missed > BufferSize) {
             // We've been lapped! The producer has overwritten all the data we were
             // supposed to read. The last_read_index is now invalid.
             // The safest thing to do is to reset our read index to the oldest available data.
-            last_read_index = (m_head.load(std::memory_order_acquire) + 1) % COMPILE_TIME_BUFFER_SIZE;
+            last_read_index = (m_head.load(std::memory_order_acquire) + 1) % BufferSize;
         }
 
         size_t current_head = m_head.load(std::memory_order_acquire);
         size_t samples_consumed = 0;
         while(last_read_index != current_head && samples_consumed < max_samples) {
             sample_buffer[samples_consumed++] = m_buffer[last_read_index];
-            last_read_index = (last_read_index + 1) % COMPILE_TIME_BUFFER_SIZE;
+            last_read_index = (last_read_index + 1) % BufferSize;
         }
 
         last_seen_count = current_update_count;
@@ -95,89 +99,16 @@ public:
         // The latest data is at the index just before the current head.
         // We use memory_order_acquire to ensure we see the data written by the producer.
         size_t current_head = m_head.load(std::memory_order_acquire);
-        size_t latest_index = (current_head == 0) ? COMPILE_TIME_BUFFER_SIZE - 1 : current_head - 1;
+        size_t latest_index = (current_head == 0) ? BufferSize - 1 : current_head - 1;
         latest_data = m_buffer[latest_index];
         return true;
     }
 
-    // Stub for the vector-based consume, which should not be used in firmware builds.
-    // Leaving it unimplemented will cause a linker error if it's ever called.
-    bool consume(std::vector<T>&, unsigned int&, size_t&) {
-        // This should never be called in a firmware build.
-        // If you get a linker error pointing here, it means a Consumer
-        // is incorrectly trying to use a std::vector on firmware.
-        return false;
-    }
-
-#else 
-    // --- Desktop/SITL, Mutex-based, Dynamic Implementation ---
-    explicit DataChannel(size_t buffer_size) : m_update_count(0), m_buffer_size(buffer_size) {}
-
-    // Public API declarations - available for both builds
-    size_t consume(T* sample_buffer, size_t max_samples, unsigned int& last_seen_count, size_t& last_read_index);
-    bool consume(std::vector<T>& samples, unsigned int& last_seen_count, size_t& last_read_index);
-
-    void post(const T& data) {
-        // On desktop, we can safely throw an exception for NaN data.
-        if constexpr (detail::is_nan_checkable_v<T>) {
-            if (data.containsNaN()) {
-                throw std::invalid_argument("NaN detected in posted data of type: " + std::string(typeid(T).name()));
-            }
-        }
-
-        std::lock_guard<std::mutex> lock(m_mutex);
-        m_buffer.push_back(data);
-        if (m_buffer.size() > m_buffer_size) {
-            m_buffer.pop_front();
-        }
-        m_update_count++;
-    }
-
-    bool consume(std::vector<T>& samples, unsigned int& last_seen_count, size_t& /*last_read_index*/) {
-        std::lock_guard<std::mutex> lock(m_mutex);
-        if (last_seen_count >= m_update_count) {
-            return false;
-        }
-
-        size_t new_samples_count = m_update_count - last_seen_count;
-        size_t num_to_copy = std::min(new_samples_count, m_buffer.size());
-        
-        samples.assign(m_buffer.end() - num_to_copy, m_buffer.end());
-
-        last_seen_count = m_update_count;
-        return true;
-    }
-
-    bool getLatest(T& latest_data) {
-        std::lock_guard<std::mutex> lock(m_mutex);
-        if (!m_buffer.empty()) {
-            latest_data = m_buffer.back();
-            return true;
-        }
-        return false;
-    }
-
-    // Stub for the raw-pointer-based consume, which should not be used in desktop builds.
-    size_t consume(T*, size_t, unsigned int&, size_t&) {
-        // This should never be called in a non-firmware build.
-        // If you get a linker error here, something is wrong in the Consumer/DataManager logic.
-        return 0;
-    }
-#endif
-
 private:
-#ifdef FIRMWARE_BUILD
     // For embedded, we use a std::array to prevent dynamic allocation.
     // The size must be known at compile time. We'll use a generous default.
     // NOTE: This requires all DataChannels to have the same size.
-    static constexpr size_t COMPILE_TIME_BUFFER_SIZE = 50;
-    std::array<T, COMPILE_TIME_BUFFER_SIZE> m_buffer;
+    std::array<T, BufferSize> m_buffer;
     std::atomic<size_t> m_head;
     std::atomic<unsigned int> m_update_count;
-#else
-    std::deque<T> m_buffer;
-    unsigned int m_update_count;
-    size_t m_buffer_size;
-    std::mutex m_mutex; // Each channel now has its own mutex
-#endif
 };
