52ede9842fdd89f665437aea35ee3347b3c324d8
Alex the Rizzler <gunningpwner82@gmail.com>
2026-02-11 17:51:23 -0600

    some pseudo code..... this coders block is real.

--- lib/flight_core/src/KinematicAdjudicator.cpp
+++ lib/flight_core/src/KinematicAdjudicator.cpp
@@ -4,7 +4,21 @@ KinematicAdjuticator::KinematicAdjuticator() :
 m_estimator(model),
 m_indicator(model)
 {
-    // initialize model and signals here
     
 };
 
+void KinematicAdjuticator::update(){
+ // Method to be called upon receipt of new imu data,
+ // 
+ // Filter new imu measurement
+ 
+ // if state is learning
+        // give to estimator
+        // calculate new excitation command
+        //send to motors
+ // else
+    // give to indi controller
+    // get output from indi controller
+    // send to motors
+
+};
